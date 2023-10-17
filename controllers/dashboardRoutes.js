4e94699fdfb441b9fd873b54756df118f8e26ee0
rddaps <dapa0002@umn.edu>
2023-10-16 19:44:10 -0500

    fine tuning data & starting handlebars

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -46,7 +46,7 @@ router.get('/blog/:id', async (req, res) => {
   }
 });
 
-router.get('/profile', withAuth, async (req, res) => {
+router.get('/dashboard', withAuth, async (req, res) => {
   try {
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
@@ -55,7 +55,7 @@ router.get('/profile', withAuth, async (req, res) => {
 
     const user = userData.get({ plain: true });
 
-    res.render('profile', {
+    res.render('dashboard', {
       ...user,
       logged_in: true
     });
---
+++ public/js/dashboard.js
@@ -0,0 +1,46 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+  
+const title = document.querySelector('#blog-title').value.trim();
+const contents = document.querySelector('#blog-contents').value.trim();
+
+if (title && contents) {
+  const response = await fetch(`/api/blogs`, {
+    method: 'POST',
+    body: JSON.stringify({ title, contents }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert('Failed to create blog');
+    }
+  }
+};
+
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('content-id')) {
+    const id = event.target.getAttribute('content-id');
+
+    const response = await fetch(`/api/blogs/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to delete blog');
+    }
+  }
+};
+
+document
+  .querySelector('.new-blog-form')
+  .addEventListener('submit', newFormHandler);
+
+document
+  .querySelector('.blog-list')
+  .addEventListener('click', delButtonHandler);
